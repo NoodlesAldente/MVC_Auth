f4de937dbd1dafb469867f8177f19d403f1fad9b
bob_ecureuil <thomasgraulle@gmail.com>
2020-08-26 21:09:25 +0200

    Correction de la base de données:   - Quelques modifications effectué pour que la base de données fonctionne correctement   - Définition du nom de la base de données par défaut
      - Quelques modification sur le fichier todolist.txt

--- db/database.sql
+++ db/database.sql
@@ -1,10 +1,9 @@
 -- ============================================================
--- base de données : todochange
+-- base de données : mvc_db
 -- type : MySQL V5.0 (non en vrai je sais pas)
 -- date de création : aout 2020
 -- auteurs : Vincent Viannay
 -- ============================================================
-
 START TRANSACTION;
 
 -- ============================================================================
@@ -16,34 +15,29 @@ SET @defautMotDePasse = "1xxxxxx!";
 --   Suppression et création de la base de données
 -- ============================================================
 
-DROP DATABASE IF EXISTS todochange;
-CREATE DATABASE todochange;
-USE todochange;
+DROP DATABASE IF EXISTS mvc_db;
+CREATE DATABASE mvc_db;
+USE mvc_db;
 
 CREATE TABLE user (
    id_user              BIGINT UNSIGNED AUTO_INCREMENT,
    pseudo_user          VARCHAR(40) NOT NULL,
-   password_user        VARCHAR(120) NOT NULL,
+   password_user        VARCHAR(130) NOT NULL,
    grain_de_sel_user    VARCHAR(20) NOT NULL,
    CONSTRAINT PK_user PRIMARY KEY (id_user)
 )  ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE PROCEDURE ajoutUser(IN ppseudo_user VARCHAR(40),
-						   IN ppassword_user VARCHAR(120)
-BEGIN
-	-- génération du grain de sel aléatoire
-	DECLARE vGrainDeSel VARCHAR(20);
-	SET vGrainDeSel = getGrainDeSel();
+-- ============================================================================
+--   modification du délimiter
+-- ============================================================================
+DELIMITER //
 
-	-- insertion de l'utilisateur
-    INSERT INTO user VALUES (pLoginUser,
-	                         getMotDePasseHache(pMotDePasseUser, vGrainDeSel),
-							 vGrainDeSel);
-END //
-
+-- ============================================================================
+--   fonction
+-- ============================================================================
 CREATE FUNCTION getMotDePasseHache(ppassword_user VARCHAR(40), ppgrain_de_sel_user VARCHAR(20)) RETURNS VARCHAR(128)
 BEGIN
-	RETURN SHA2(CONCAT(MD5(CONCAT(pGrainDeSel, pMotDePasse)), pGrainDeSel, MD5(CONCAT(pMotDePasse, pGrainDeSel))), 512);
+	RETURN SHA2(CONCAT(MD5(CONCAT(ppgrain_de_sel_user, ppassword_user)), ppgrain_de_sel_user, MD5(CONCAT(ppassword_user, ppgrain_de_sel_user))), 512);
 END //
 
 CREATE FUNCTION getUnCaractereAleatoire() RETURNS VARCHAR(1)
@@ -52,6 +46,36 @@ BEGIN
 	SET vChaine = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	RETURN SUBSTR(vChaine, CEILING(RAND() * LENGTH(vChaine)), 1);
 END //
+
+-- ============================================================================
+--   fonction renvoyant un grain de sel aléatoire
+-- ============================================================================
+CREATE FUNCTION getGrainDeSel() RETURNS VARCHAR(20)
+BEGIN
+	DECLARE vGrainDeSel VARCHAR(20);
+	SET vGrainDeSel = "";
+	WHILE LENGTH(vGrainDeSel) < 20 DO
+       	SET vGrainDeSel = CONCAT(vGrainDeSel, getUnCaractereAleatoire());
+	END WHILE;
+	RETURN vGrainDeSel;
+END //
+
+
+CREATE PROCEDURE ajoutUser(IN ppseudo_user VARCHAR(40),
+						   IN ppassword_user VARCHAR(120))
+BEGIN
+	-- génération du grain de sel aléatoire
+	DECLARE vGrainDeSel VARCHAR(20);
+	SET vGrainDeSel = getGrainDeSel();
+
+	-- insertion de l'utilisateur
+    INSERT INTO user (pseudo_user, password_user, grain_de_sel_user) VALUES (ppseudo_user,
+	                         getMotDePasseHache(ppassword_user, vGrainDeSel),
+							 vGrainDeSel);
+END //
+
+
+DELIMITER ;
 
 -- ===============================================================================================================
 --   insertion des enregistrements relatifs aux utilisateurs
